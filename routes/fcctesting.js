233f4d359b3167a356a56b1be65e26fd7af2b9e0
manishh28 <156950410+manishh28@users.noreply.github.com>
2026-06-13 13:54:24 +0530

    Update fcctesting.js

--- routes/fcctesting.js
+++ routes/fcctesting.js
@@ -10,12 +10,27 @@ module.exports = function (app) {
 
   app.route('/_api/get-tests').get(function (req, res) {
     const Mocha = require('mocha');
-    const mocha = new Mocha({ timeout: 10000 });
+    const mocha = new Mocha({ timeout: 10000, reporter: 'min' });
+
+    // Clear cached test file so it re-runs fresh each time
+    delete require.cache[require.resolve('../tests/2_functional-tests.js')];
+
     mocha.addFile('./tests/2_functional-tests.js');
+
     const output = [];
+
+    // Make Mocha globals available
+    const context = mocha.suite.ctx;
+
     mocha.run()
-      .on('pass', test => output.push({ title: test.fullTitle(), state: 'passed' }))
-      .on('fail', (test, err) => output.push({ title: test.fullTitle(), state: 'failed', err: err.message }))
-      .on('end', () => res.json(output));
+      .on('pass', test => {
+        output.push({ title: test.fullTitle(), state: 'passed' });
+      })
+      .on('fail', (test, err) => {
+        output.push({ title: test.fullTitle(), state: 'failed', err: err.message });
+      })
+      .on('end', () => {
+        res.json(output);
+      });
   });
 };
