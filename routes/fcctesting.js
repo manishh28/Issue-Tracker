6804a24e3bdf052d6e6790575e80883b4505eabb
manishh28 <156950410+manishh28@users.noreply.github.com>
2026-06-13 13:50:40 +0530

    Update fcctesting.js

--- routes/fcctesting.js
+++ routes/fcctesting.js
@@ -7,4 +7,15 @@ module.exports = function (app) {
       .map(r => r.route.path);
     res.json({ routes });
   });
+
+  app.route('/_api/get-tests').get(function (req, res) {
+    const Mocha = require('mocha');
+    const mocha = new Mocha({ timeout: 10000 });
+    mocha.addFile('./tests/2_functional-tests.js');
+    const output = [];
+    mocha.run()
+      .on('pass', test => output.push({ title: test.fullTitle(), state: 'passed' }))
+      .on('fail', (test, err) => output.push({ title: test.fullTitle(), state: 'failed', err: err.message }))
+      .on('end', () => res.json(output));
+  });
 };
